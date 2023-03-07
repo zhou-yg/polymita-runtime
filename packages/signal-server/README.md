e9129f963f4540bc3eabb501eb47d503e376dee8
zhou-yg <zhou.y.g.890@gmail.com>
2023-03-07 21:43:36 +0800

    feat: rename

--- packages/signal-server/README.md
+++
@@ -1,2 +0,0 @@
-# @polymita/signal
-a fundamental state manager library for reactive driven 
--- packages/signal-server/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@polymita/signal'
-export * from './model'
-export * from './util'
-export * from './types'
