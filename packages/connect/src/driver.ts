1bb9bb859ac3de1c3703762e9117835731291d28
zhou-yg <zhou.y.g.890@gmail.com>
2023-04-01 19:00:59 +0800

    release: polymita vundefined

--- packages/connect/src/driver.ts
+++ packages/connect/src/driver.ts
@@ -1,78 +1,78 @@
 import {
   RunnerModelScope,
   IHookContext,
-  IModelIndexesBase,
-} from "@polymita/signal-model";
+  IModelIndexesBase
+} from '@polymita/signal-model'
 
-let currentModelIndexes: IModelIndexesBase | null = null;
+let currentModelIndexes: IModelIndexesBase | null = null
 export function getCurrentModelIndexes() {
-  return currentModelIndexes;
+  return currentModelIndexes
 }
 export function registerModelIndexes(indexes: IModelIndexesBase | null) {
-  currentModelIndexes = indexes;
+  currentModelIndexes = indexes
 }
 
 export class RenderDriver {
-  mode?: "collect" | "consume";
+  mode?: 'collect' | 'consume'
 
-  believeContext = false;
-  updateCallbackSync = false;
+  believeContext = false
+  updateCallbackSync = false
 
-  BMValuesMap: Map<string, RunnerModelScope<any>[]> = new Map();
+  BMValuesMap: Map<string, RunnerModelScope<any>[]> = new Map()
 
-  pushListener?: (scope: RunnerModelScope<any>) => void;
+  pushListener?: (scope: RunnerModelScope<any>) => void
 
-  consumeCache: Map<string, IHookContext[] | undefined> = new Map();
+  consumeCache: Map<string, IHookContext[] | undefined> = new Map()
 
   fromContextMap(contextMap: Record<string, IHookContext[]>) {
-    Object.keys(contextMap).forEach((bmName) => {
-      this.consumeCache.set(bmName, contextMap[bmName]);
-    });
+    Object.keys(contextMap).forEach(bmName => {
+      this.consumeCache.set(bmName, contextMap[bmName])
+    })
   }
 
   switchToServerConsumeMode() {
-    this.mode = "consume";
-    this.believeContext = true;
-    this.updateCallbackSync = false;
+    this.mode = 'consume'
+    this.believeContext = true
+    this.updateCallbackSync = false
   }
   switchToClientConsumeMode() {
-    this.mode = "consume";
-    this.believeContext = false;
-    this.updateCallbackSync = true;
+    this.mode = 'consume'
+    this.believeContext = false
+    this.updateCallbackSync = true
   }
 
   pop(name: string) {
-    return this.BMValuesMap.get(name)?.pop();
+    return this.BMValuesMap.get(name)?.pop()
   }
 
   getContext(name: string) {
-    if (this.mode !== "consume") {
-      return;
+    if (this.mode !== 'consume') {
+      return
     }
-    let r = this.consumeCache.get(name);
+    let r = this.consumeCache.get(name)
     if (!r) {
-      r = this.BMValuesMap.get(name)?.map((s) => s.createInputComputeContext());
-      this.consumeCache.set(name, r);
+      r = this.BMValuesMap.get(name)?.map(s => s.createInputComputeContext())
+      this.consumeCache.set(name, r)
     }
 
-    return r;
+    return r
   }
 
   onPush(f: (scope: RunnerModelScope<any>) => void) {
-    this.pushListener = f;
+    this.pushListener = f
   }
 
   push(scope: RunnerModelScope<any>, name: string) {
-    if (this.mode !== "collect") {
-      return;
+    if (this.mode !== 'collect') {
+      return
     }
 
-    let values = this.BMValuesMap.get(name);
+    let values = this.BMValuesMap.get(name)
     if (!values) {
-      values = [];
-      this.BMValuesMap.set(name, values);
+      values = []
+      this.BMValuesMap.set(name, values)
     }
-    this.pushListener?.(scope);
-    return values.push(scope);
+    this.pushListener?.(scope)
+    return values.push(scope)
   }
 }
