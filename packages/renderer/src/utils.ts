60d3229e2e1f29302683b051cc79d8e0c4505a89
zhou-yg <zhou.y.g.890@gmail.com>
2023-08-30 11:54:12 +0800

    feat(renderer): add internal classnames

--- packages/renderer/src/utils.ts
+++ packages/renderer/src/utils.ts
@@ -678,3 +678,44 @@ export function shouldNotRender(json: VirtualLayoutJSON) {
 export function lowerCaseType(type: LayoutStructTree["type"]) {
   return typeof type === "function" ? type : camelToLine(type);
 }
+/**
+ * fork from https://github.com/JedWatson/classnames/blob/main/index.js
+ */
+const hasOwn = {}.hasOwnProperty;
+export function classNames() {
+  var classes = [];
+
+  for (var i = 0; i < arguments.length; i++) {
+    var arg = arguments[i];
+    if (!arg) continue;
+
+    var argType = typeof arg;
+
+    if (argType === "string" || argType === "number") {
+      classes.push(arg);
+    } else if (Array.isArray(arg)) {
+      if (arg.length) {
+        var inner = classNames.apply(null, arg);
+        if (inner) {
+          classes.push(inner);
+        }
+      }
+    } else if (argType === "object") {
+      if (
+        arg.toString !== Object.prototype.toString &&
+        !arg.toString.toString().includes("[native code]")
+      ) {
+        classes.push(arg.toString());
+        continue;
+      }
+
+      for (var key in arg) {
+        if (hasOwn.call(arg, key) && arg[key]) {
+          classes.push(key);
+        }
+      }
+    }
+  }
+  return classes.join(" ");
+}
+export const classnames = classNames;
