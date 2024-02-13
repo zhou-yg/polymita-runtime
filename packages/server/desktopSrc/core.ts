a08c3d5eefb266c77cac5b6ddfa389e197e7d793
zhou-yg <zhou.y.g.890@gmail.com>
2024-02-13 18:41:37 +0800

    feat(server): exit in start electron

--- packages/server/desktopSrc/core.ts
+++ packages/server/desktopSrc/core.ts
@@ -1,11 +1,12 @@
 /**
  * run in electron runtime
  */
-import * as electron from 'electron'
+import electron from 'electron'
 import { IConfig } from '../src/config';
 import { connectModel, getDefaultRoute } from '../src/util';
 
 const { app, BrowserWindow } = electron
+console.log('electron: ', electron);
 
 function createWindow (winOptions: {
   site: string
@@ -17,11 +18,11 @@ function createWindow (winOptions: {
   mainWindow.loadURL(winOptions.site)
 }
 
-export function setupClient (c: IConfig) {
+export function setupClient (c: Partial<IConfig>) {
 
   const client = connectModel()
 
-  const defaultView = getDefaultRoute(c.pages)
+  const defaultView = c.pages ? getDefaultRoute(c.pages) : 'main'
 
   const winOption = {
     site: `http://localhost:${c.port}/${defaultView}`
--- packages/server/desktopSrc/start.ts
+++ packages/server/desktopSrc/start.ts
@@ -1,25 +1,44 @@
 /**
  * run electron in official releasing version
  */
-import { IConfig } from '../src/config';
-import { setupClient } from './core';
-import {
-  readConfig,
-  http,
-} from '../src'
+import { spawn } from 'child_process'
+import { setupClient } from './core'
+import exitHook from 'exit-hook'
 
-async function createClient () {
+async function startApp () {
   const cwd = process.cwd()
 
-  const config = await readConfig({
-    cwd,
-    isProd: true,
+  const instance = spawn(
+    'tarat', ['start'], 
+    {
+      cwd: cwd,
+      stdio: ['inherit', 'pipe', 'inherit'] 
+    }
+  )
+
+  instance.on('exit', () => {
+    process.exit()
   })
 
-  await http.createServer(config)
-  
-  const client = setupClient(config)
-  client.start()
+  instance.stdout.pipe(process.stdout)
+
+  exitHook(() => {
+    instance.kill('SIGHUP')
+  })
+
+  return new Promise<void>(resolve => {
+    instance.stdout.on('data', (e2) => {
+      if (e2.toString().includes('connect success')) {
+        resolve()
+      }
+    })
+  })
 }
 
-createClient()
+function createElectron () {
+  const client = setupClient({
+    port: 9100,
+  })
+  client.start()
+}
+startApp().then(() => new Promise<void>(resolve => setTimeout(() => resolve(), 5000))).then(createElectron)
