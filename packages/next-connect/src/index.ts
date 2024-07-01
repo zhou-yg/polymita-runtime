0a2f88ed915481697ce9fe93235d21c51bd4d77b
zhou-yg <zhou.y.g.890@gmail.com>
2024-07-02 00:36:31 +0800

    feat(nc):  classify signal and react-hooks

--- packages/next-connect/src/index.ts
+++ packages/next-connect/src/index.ts
@@ -1,54 +1,5 @@
-import {
-  Driver,
-  getNamespace,
-  IHookContext,
-  IModelIndexesBase,
-  ModelRunner,
-  Plugin,
-} from "@polymita/signal-model";
+export * from "./signal/signalHooks";
+export * from "./signal/connect";
+export * from "./signal/getContext";
+export * from "./react-hooks/hooks";
 
-export * from "./signalHooks";
-export * from "./connect";
-export * from "./hooks";
-
-export function createGetContext(p: {
-  modelIndexes: IModelIndexesBase;
-  createPlugin: () => Plugin;
-}) {
-  const { modelIndexes, createPlugin } = p;
-
-  async function getContext<T extends Driver>(
-    signal: Driver,
-    ...args: Parameters<T>
-  ) {
-    const namespace = getNamespace(signal);
-    const isComposedDriver = !!(signal as any).__polymita_compose__;
-
-    console.log(
-      "namespace && mi && isComposedDriver: ",
-      namespace,
-      isComposedDriver,
-    );
-
-    const runner = new ModelRunner(signal, {
-      modelIndexes:
-        namespace && modelIndexes && isComposedDriver
-          ? (modelIndexes[namespace] as IModelIndexesBase)
-          : modelIndexes,
-      plugin: createPlugin(),
-      believeContext: false,
-      runtime: typeof window === "undefined" ? "nodejs" : "edge",
-    });
-
-    const result: ReturnType<T> = runner.init(args);
-
-    await runner.ready();
-
-    return [result, runner.scope.createBaseContext()] as [
-      ReturnType<T>,
-      IHookContext,
-    ];
-  }
-
-  return getContext;
-}
