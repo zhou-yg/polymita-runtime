c8d69a59ba1950df4cc3ae7bf0876c68e6baef9f
zhou-yg <zhou.y.g.890@gmail.com>
2024-10-08 01:03:54 +0800

    feat(nc): add zip log

--- packages/next-server/cli/zip.ts
+++ packages/next-server/cli/zip.ts
@@ -1,8 +1,10 @@
-import { zipOutput } from '../src'
+import { logFrame, zipOutput } from '../src'
 import build from './build'
 
 export default async (cwd: string) => {
   const config = await build(cwd, false)
 
   await zipOutput(config)
+
+  logFrame('zip success at:', config.pointFiles.output.zip)
 }
